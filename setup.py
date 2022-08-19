091da5116c0006cb36e97f0b5f2ebd39d48cedc3
anascacais <anascacais@gmail.com>
2022-08-19 15:40:57 +0100

    Changes package name

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 from setuptools import setup
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 DESCRIPTION = "ScientISST-NButils is a package for management of the ScientISST Notebooks repository."
 
 setup(
-    name="scientisst-NButils",
+    name="scientisstNButils",
     version=VERSION,
     description=DESCRIPTION,
     url="https://github.com/scientisst/scientisst-NButils.git",
     author="ScientISST",
     author_email="developer@scientisst.com",
     license="MIT",
-    packages=["scientisst-NButils"],
+    packages=["scientisstNButils"],
     zip_safe=False,
 )
