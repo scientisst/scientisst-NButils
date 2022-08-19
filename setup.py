bd70cde818db3eb9db9786ade462719fb66ec2d9
anascacais <anascacais@gmail.com>
2022-08-19 15:28:58 +0100

    Adds setup.py to pakcage project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="scientisst-NButils",
+    version="0.0.1",
+    description="ScientISST-NButils is a package for management of the ScientISST Notebooks repository.",
+    url="https://github.com/scientisst/scientisst-NButils.git",
+    author="ScientISST",
+    author_email="developer@scientisst.com",
+    license="MIT",
+    packages=["scientisst-NButils"],
+    zip_safe=False,
+)
