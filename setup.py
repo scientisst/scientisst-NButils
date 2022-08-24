b51b1a55bb22cd988db38c5e867d010f5f151085
anascacais <anascacais@gmail.com>
2022-08-24 15:33:34 +0100

    Update package version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 DESCRIPTION = "ScientISST-NButils is a package for management of the ScientISST Notebooks repository."
 
 setup(
